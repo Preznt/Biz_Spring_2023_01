fed2f0ed6b5aa4a3d2c10d7f10fa8999f812eade
Preznt <bjw1403@gmail.com>
2023-02-08 11:14:20 +0900

    naver books

--- SpMVC_006_ContactA/contact(2022-02-01).sql
+++ SpMVC_006_ContactA/contact(2022-02-01).sql
@@ -9,12 +9,23 @@ DROP TABLE tbl_contact;
 
 
 CREATE TABLE tbl_contact (
-	c_uid varchar(256) NOT NULL,
-	c_name varchar(125),
-	c_tel  varchar(30),
-	c_addr  varchar(125),
-	c_hobby  varchar(125)
+	c_uid	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
+c_name	VARCHAR(20)	NOT NULL	,
+c_tel	VARCHAR(15)	NOT NULL	,
+c_addr	VARCHAR(125)	,
+c_hobby	VARCHAR(20)
+	
 );
+
+CREATE TABLE tbl_hobbies (
+h_uid	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
+h_cuid	BIGINT	NOT NULL	,
+h_name	VARCHAR(15)	NOT NULL	
+
+);
+
+DROP TABLE tbl_contact;
+DROP TABLE tbl_hobbies;
 
 INSERT INTO tbl_contact (
 	c_uid, c_name, c_tel, c_addr, c_hobby
